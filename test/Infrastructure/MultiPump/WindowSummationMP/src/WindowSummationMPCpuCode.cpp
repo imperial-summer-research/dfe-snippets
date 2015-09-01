bf438ec6e96035d6e87b9c64366f927677dfbd52
Vincent Zhao <vincentzhaorz@gmail.com>
2015-09-01 21:09:03 +0100

    updated the directory structure and data, and also the kernel code

--- test/Infrastructure/MultiPump/WindowSummationMP/src/WindowSummationMPCpuCode.cpp
+++ test/Infrastructure/MultiPump/WindowSummationMP/src/WindowSummationMPCpuCode.cpp
@@ -28,6 +28,7 @@ int main(int argc, char *argv[]) {
     int W = WindowSummationMP_W;
     int C = WindowSummationMP_C;
     int depth = WindowSummationMP_depth;
+    int iter = 100;
     LL N = 8192 * 8192;
 
     index_t *index  = (index_t *) malloc(sizeof(index_t) * N);
@@ -45,19 +46,43 @@ int main(int argc, char *argv[]) {
     printf("depth:\t %d\n", depth);
     printf("N:\t %lld\n", N);
 
+    max_file_t *mf = WindowSummationMP_init();
+    max_engine_t *me = max_load(mf, "*");
+
+    WindowSummationMP_Init_actions_t init_actions;
+    value_rom_t **roms = (value_rom_t **) &init_actions.inmem_WindowSummationMPGatherKernel_ROM0000;
+    for (int i = 0; i < W/C/2; i++) {
+        roms[i] = (value_rom_t *) malloc(sizeof(value_rom_t) * depth);
+        memcpy(roms[i], rom, sizeof(value_rom_t) * depth);
+    }
+
+    WindowSummationMP_Compute_actions_t actions;
+    actions.param_length = N;
+    actions.instream_index = index;
+    actions.outstream_result = result;
+    
+    printf("Setting ROM ...\n");
+    WindowSummationMP_Init_run(me, &init_actions);
+
     printf("Heating DFE ...\n");
-    WindowSummationMP(N, index, result, rom);
+    WindowSummationMP_Compute_run(me, &actions);
 
     printf("Running DFE ...\n");
     struct timeval t0, t1;
     gettimeofday(&t0, 0);
-    WindowSummationMP(N, index, result, rom);
+    for (int i = 0; i < iter; i++)
+        WindowSummationMP_Compute_run(me, &actions);
     gettimeofday(&t1, 0);
+
+    max_unload(me);
+
     double duration = (double)(t1.tv_sec-t0.tv_sec)+(double)(t1.tv_usec-t0.tv_usec)/1e6;
-    printf("Total time %lf s per element time %lf us freq %.2f MHz bandwidth %.2f MB/s\n", 
+    duration /= iter;
+    printf("Total time %lf s per element time %lf us freq(elem) %.2f MHz freq(tick) %.2f MHz bandwidth %.2f MB/s\n", 
         duration, 
         duration * 1e6 / N,
         1.0 / (duration / N) / 1e6,
+        1.0 / (duration / (N/W)) / 1e6,
         sizeof(value_t) / (duration / N) / 1e6);
 
     for (int i = 0; i < N/W; i++) {
